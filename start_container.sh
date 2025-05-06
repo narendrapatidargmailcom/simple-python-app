1e13d9e869f599c490d0f33e92564f74bdfe74f6
narendrapatidargmailcom <126964744+narendrapatidargmailcom@users.noreply.github.com>
2025-05-06 15:33:36 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo
+docker pull narendrasanewa/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+
+docker -d -p 5000:5000 narendrasanewa/simple-python-flask-app
